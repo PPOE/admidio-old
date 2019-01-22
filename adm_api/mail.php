9e9b32fabebbb2c915fc7fd64b3317f918e3b01e
zevenom <zevenom@gmail.com>
2019-01-22 21:02:04 +0100

    Update mail.php

--- adm_api/mail.php
+++ adm_api/mail.php
@@ -1,16 +1,12 @@
 <?php
-require("config.php");
+
 function mail_utf8($to, $subject, $message, $headers)
 {
   $to = base64_encode($to);
   $subject = base64_encode($subject);
   $message = base64_encode($message);
   $headers = base64_encode($headers);
-  echo($g_adm_srv);
-  echo($g_adm_usr);
-  $link = mysqli_connect($g_adm_srv,$g_adm_usr,$g_adm_pw);
-  mysqli_select_db($link,$g_adm_db);
-  mysql_query("INSERT INTO ppoe_api_data.mail_queue (mto,msubject,mbody,mheaders) VALUES ('$to', '$subject', '$message', '$headers');");
+  mysqli_query("INSERT INTO ppoe_api_data.mail_queue (mto,msubject,mbody,mheaders) VALUES ('$to', '$subject', '$message', '$headers');");
   return true;
 }
 
